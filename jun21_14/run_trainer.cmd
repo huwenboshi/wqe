26896460d68c1b375e60ca4ee8d993270c53a17e
huwenboshi <shihuwenbo@ucla.edu>
2014-06-21 23:50:39 -0700

    updated to include the cvxopt python path

--- jun21_14/run_trainer.cmd
+++ jun21_14/run_trainer.cmd
@@ -7,6 +7,8 @@
 #$ -e ./output
 #$ -o ./output
 
+export PYTHONPATH=/u/home/s/shihuwen/project/wqe/software/cvxopt-1.1.7/lib/python2.7/site-packages:/u/home/s/shihuwen/project/wqe/software/cvxopt-1.1.7:/u/home/eeskin/shihuwen/software/ghmm/ghmm_python/lib64/python2.6/site-packages:$PYTHONPATH
+
 module load python/2.7
 
 time ./run_trainer.sh
