63c2a3d1bc227ba29f089688e91ec07e203ff844
huwenboshi <shihuwenbo@ucla.edu>
2014-06-22 00:02:15 -0700

    updated again, have to use python 2.7.3 on hoffman2

--- jun21_14/run_trainer.cmd
+++ jun21_14/run_trainer.cmd
@@ -10,6 +10,6 @@
 export PYTHONPATH=/u/home/s/shihuwen/project/wqe/software/cvxopt-1.1.7/lib/python2.7/site-packages:/u/home/s/shihuwen/project/wqe/software/cvxopt-1.1.7:/u/home/eeskin/shihuwen/software/ghmm/ghmm_python/lib64/python2.6/site-packages:$PYTHONPATH
 
 source /u/local/Modules/default/init/modules.sh
-module load python/2.7
+module load python/2.7.3
 
 time ./run_trainer.sh
