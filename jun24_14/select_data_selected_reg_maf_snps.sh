1217f0ab37f0584239e1126ced22d2b445fafe2a
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-06-24 14:26:51 -0700

    added scripts for selecting data for selected snps

---
+++ jun24_14/select_data_selected_reg_maf_snps.sh
@@ -0,0 +1,5 @@
+../scripts/parse_selected_training_data.py \
+    -f /u/home/s/shihuwen/project/wqe/affy100k_training_data_full/affy100_hind_training_full.txt \
+    -t 33 \
+    -v 67 \
+    -s selected_reg_maf_snps.txt
