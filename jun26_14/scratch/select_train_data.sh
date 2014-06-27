52f06100667a72a9818c86c57c9376f363b6361d
huwenboshi <shihuwenbo@ucla.edu>
2014-06-26 17:45:35 -0700

    added progress made today

---
+++ jun26_14/scratch/select_train_data.sh
@@ -0,0 +1,6 @@
+../prep_scripts/parse_selected_training_data.py \
+    -f /u/home/s/shihuwen/project/wqe/affy100k_training_data_full/affy100_hind_training_full.txt \
+    -t 100 \
+    -v 0 \
+    -s selected_snps.txt
+
