a53308c4951112f56dd296138988d84504f27da4
huwenboshi <shihuwenbo@ucla.edu>
2014-06-28 22:53:11 -0700

    added scripts to study problem snps

---
+++ genotype_caller/run_loo_snps.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python
+
+from socal_caller import *
+from cvxopt import matrix
+from optparse import OptionParser
+from random import shuffle
+from socal_trainer import *
+import sys
+import math
+import time
+
+# get command line
+parser = OptionParser()
+parser.add_option("-f", "--fullfile", dest="fullfile")
+parser.add_option("-o", "--outfile", dest="outfile")
+parser.add_option("-a", "--aval", dest="aval_str")
+parser.add_option("-b", "--bval", dest="bval_str")
+parser.add_option("-c", "--cval", dest="cval_str")
+parser.add_option("-d", "--dval", dest="dval_str")
+parser.add_option("-e", "--eval", dest="eval_str")
+parser.add_option("-s", "--snps", dest="snps")
+(options, args) = parser.parse_args()
+fullfile_nm = options.fullfile
+outfile_nm = options.outfile
+snps_nm = options.snps
+aval = options.aval_str
+bval = options.bval_str
+cval = options.cval_str
+dval = options.dval_str
+evalu = options.eval_str
+
+# check command line
+if(fullfile_nm == None or outfile_nm == None or
+   aval == None or bval == None or cval == None or
+   dval == None or evalu == None or snps_nm == None):
+    sys.stderr.write("Usage:\n")
+    sys.stderr.write("\tUse -f to specify fullfile\n")
+    sys.stderr.write("\tUse -o to specify outfile\n")
+    sys.stderr.write("\tUse -a to specify aval\n")
+    sys.stderr.write("\tUse -b to specify bval\n")
+    sys.stderr.write("\tUse -c to specify cval\n")
+    sys.stderr.write("\tUse -d to specify dval\n")
+    sys.stderr.write("\tUse -e to specify eval\n")
+    sys.stderr.write("\tUse -s to specify snps file\n")
+    sys.exit()
+
+# load in snps
+snps_set = set()
+snps_file = open(snps_nm, 'r')
+for line in snps_file:
+    line = line.strip()
+    snps_set.add(line)
+snps_file.close()
+
+# files for saving the result
+outfile = open(outfile_nm,'w')
+
+# parameters
+c1 = float(aval)
+c2 = float(bval)
+c3 = float(cval)
+c4 = float(dval)
+c5 = float(evalu)
+
+# read in data
+indv_list = []
+fullfile = open(fullfile_nm, 'r')
+flr = False
+for line in fullfile:
+
+    # get line
+    line = line.strip()
+    cols = line.split()
+    
+    # get individuals
+    if(flr == False):
+        flr = True
+        indv_list = cols
+        continue
+    indv_geno = dict()
+
+    # get snp id
+    snp = cols[0]
+
+    if(snp not in snps_set):
+        continue
+    
+    # get snp information - alleles and frequency
+    sensea = cols[1][0]
+    senseb = cols[1][2]
+    freq = cols[2]
+    
+    outline = snp+'\t'+freq+'\t'
+    
+    paa = []
+    pab = []
+    pbb = []
+    
+    # parse out genotype information
+    naa = 0
+    nab = 0
+    nbb = 0
+    used_indv = []
+    for i in xrange(len(indv_list)):
+        info = cols[i+3].split(':')
+        geno = info[1]
+        sensea_int = info[0].split('/')[0]
+        senseb_int = info[0].split('/')[1]
+        if(geno == 'NN'):
+            continue
+        else:
+            used_indv.append(indv_list[i])
+            if(geno == sensea+sensea):
+                naa += 1
+                indv_geno[indv_list[i]] = (sensea_int,senseb_int,'aa')
+                paa.append((sensea_int,senseb_int))
+            elif(geno == sensea+senseb or geno == senseb+sensea):
+                nab += 1
+                indv_geno[indv_list[i]] = (sensea_int,senseb_int,'ab')
+                pab.append((sensea_int,senseb_int))
+            else:
+                nbb += 1
+                indv_geno[indv_list[i]] = (sensea_int,senseb_int,'bb')
+                pbb.append((sensea_int,senseb_int))
+    
+    for p in paa:
+        print '%s,%s,%s' % (str(p[0]),str(p[1]),'1')
+    for p in pab:
+        print '%s,%s,%s' % (str(p[0]),str(p[1]),'2')
+    for p in pbb:
+        print '%s,%s,%s' % (str(p[0]),str(p[1]),'3')
+    
+    outline += str(naa)+','+str(nab)+','+str(nbb)+'\n'
+    outline += ','.join(used_indv)+'\n'
+    outfile.write(outline)
+    
+    # get training individuals and validation individuals
+    for i in xrange(len(used_indv)):
+    
+        # select training individuals and leave-one-out validating individual
+        tindv = used_indv[0:i]+used_indv[i+1:]
+        vindv = used_indv[i]
+        
+        # train socal
+        paa = []
+        pab = []
+        pbb = []
+        for ind in tindv:
+            info = indv_geno[ind]
+            genotype = info[2]
+            if(genotype == 'aa'):
+                paa.append([float(info[0]), float(info[1])])
+            elif(genotype == 'ab'):
+                pab.append([float(info[0]), float(info[1])])
+            elif(genotype == 'bb'):
+                pbb.append([float(info[0]), float(info[1])])
+        
+        trainer = socal_trainer(snp, paa, pab, pbb, c1, c2, c3)
+        
+        # time execution in ms
+        t1 = time.time()
+        trainer.train()
+        trainer.rescue(c5)
+        t2 = time.time()
+        dt = (t2-t1)*1000.0
+        
+        # get truth
+        info = indv_geno[vindv]
+        truth = info[2]
+        
+        # get the ellipsoids - skip the ellipsoid if any cluster can't be
+        # estimated
+        ellipsoids = trainer.get_ellipsoids()
+        e_aa = ellipsoids['aa']
+        e_ab = ellipsoids['ab']
+        e_bb = ellipsoids['bb']
+        if(e_aa == None or e_ab == None or e_bb == None):
+            outfile.write(truth+'/NN')
+            if(i != len(used_indv)-1):
+                outfile.write('\n')
+            continue
+        
+        # run the caller
+        c_aa = e_aa['c']
+        E_aa = e_aa['E']
+        c_ab = e_ab['c']
+        E_ab = e_ab['E']
+        c_bb = e_bb['c']
+        E_bb = e_bb['E']
+        sc = socal_caller(c_aa,E_aa,c_ab,E_ab,c_bb,E_ab,c4)
+        
+        # do validation
+        x = matrix([float(info[0]),float(info[1])],(2,1))
+        result = sc.mindist(x)
+        
+        if(result == None):
+            outfile.write(truth+'/NN')
+            if(i != len(used_indv)-1):
+                outfile.write('\n')
+            continue
+        
+        call = result[0]
+        score = result[1]
+        
+        # write out result
+        outfile.write(truth+'/'+call+','+str(score)+','+str(dt))
+        outfile.write('\n')
+    outfile.write('\n')
+        
+fullfile.close()
+outfile.close()
