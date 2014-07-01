294ccdfa81288fcae742e614c5fa7bf2201ed49e
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-07-01 12:06:25 -0700

    added data for plot in method section

--- method_figs/gen_defined_cluster.m
+++ method_figs/gen_defined_cluster.m
@@ -1,5 +1,15 @@
-data = dlmread('SNP_A-1722680int.txt');
+data = dlmread('SNP_A-1721275int.txt');
+train_data = dlmread('SNP_A-1721275_train.txt');
+
 figure;
+
+% plot all data point
 plot(data(:,1),data(:,2),'b.');
-xlabel('log(allele A intensity)');
-ylabel('log(allele B intensity)');
+hold on;
+
+% plot aa training data
+plot(train_data(:,1),train_data(:,2),'r.');
+hold on;
+
+xlabel('log(allele A intensity)', 'FontSize', 20);
+ylabel('log(allele B intensity)', 'FontSize', 20);
