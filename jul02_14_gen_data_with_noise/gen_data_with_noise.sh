1ac1bef2decce4a980dee3c60877758a2ecaad73
huwenboshi <shihuwenbo@ucla.edu>
2014-07-02 16:05:29 -0700

    added scripts to generate and split data with noise

---
+++ jul02_14_gen_data_with_noise/gen_data_with_noise.sh
@@ -0,0 +1,41 @@
+../sim_outlier_scripts/sim_gauss_noise.py \
+    -f ../affy100k_training_data_full/affy100_xba_training_full.txt \
+    -o ../affy100k_training_data_noise_0.0/affy100_xba_training_noise_0.0.txt \
+    -s snps_defined_cluster.txt \
+    -v 0.0
+
+../sim_outlier_scripts/sim_gauss_noise.py \
+    -f ../affy100k_training_data_full/affy100_xba_training_full.txt \
+    -o ../affy100k_training_data_noise_0.5/affy100_xba_training_noise_0.5.txt \
+    -s snps_defined_cluster.txt \
+    -v 0.5
+
+../sim_outlier_scripts/sim_gauss_noise.py \
+    -f ../affy100k_training_data_full/affy100_xba_training_full.txt \
+    -o ../affy100k_training_data_noise_1.0/affy100_xba_training_noise_1.0.txt \
+    -s snps_defined_cluster.txt \
+    -v 1.0
+
+../sim_outlier_scripts/sim_gauss_noise.py \
+    -f ../affy100k_training_data_full/affy100_xba_training_full.txt \
+    -o ../affy100k_training_data_noise_1.5/affy100_xba_training_noise_1.5.txt \
+    -s snps_defined_cluster.txt \
+    -v 1.5
+
+../sim_outlier_scripts/sim_gauss_noise.py \
+    -f ../affy100k_training_data_full/affy100_xba_training_full.txt \
+    -o ../affy100k_training_data_noise_2.0/affy100_xba_training_noise_2.0.txt \
+    -s snps_defined_cluster.txt \
+    -v 2.0
+
+../sim_outlier_scripts/sim_gauss_noise.py \
+    -f ../affy100k_training_data_full/affy100_xba_training_full.txt \
+    -o ../affy100k_training_data_noise_2.5/affy100_xba_training_noise_2.5.txt \
+    -s snps_defined_cluster.txt \
+    -v 2.5
+
+../sim_outlier_scripts/sim_gauss_noise.py \
+    -f ../affy100k_training_data_full/affy100_xba_training_full.txt \
+    -o ../affy100k_training_data_noise_3.0/affy100_xba_training_noise_3.0.txt \
+    -s snps_defined_cluster.txt \
+    -v 3.0
---
+++ jul02_14_gen_data_with_noise/split_data_with_noise.sh
@@ -0,0 +1,7 @@
+../prep_scripts/split_train_data.py -f ../affy100k_training_data_noise_0.0/affy100_xba_training_noise_0.0.txt -n 10
+../prep_scripts/split_train_data.py -f ../affy100k_training_data_noise_0.5/affy100_xba_training_noise_0.5.txt -n 10
+../prep_scripts/split_train_data.py -f ../affy100k_training_data_noise_1.0/affy100_xba_training_noise_1.0.txt -n 10
+../prep_scripts/split_train_data.py -f ../affy100k_training_data_noise_1.5/affy100_xba_training_noise_1.5.txt -n 10
+../prep_scripts/split_train_data.py -f ../affy100k_training_data_noise_2.0/affy100_xba_training_noise_2.0.txt -n 10
+../prep_scripts/split_train_data.py -f ../affy100k_training_data_noise_2.5/affy100_xba_training_noise_2.5.txt -n 10
+../prep_scripts/split_train_data.py -f ../affy100k_training_data_noise_3.0/affy100_xba_training_noise_3.0.txt -n 10
