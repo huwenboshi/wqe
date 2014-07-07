b484d136adc96bf5f47609b97f70b889f4e394c7
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-07-07 12:47:07 -0700

    added results for different beta choices

--- result_figs/cmp_socal_hapmap/gen_cr_vs_ac_plot.m
+++ result_figs/cmp_socal_hapmap/gen_cr_vs_ac_plot.m
@@ -1,8 +1,18 @@
-tbl = dlmread('cr_vs_ac.txt');
-tbl(:,2) = tbl(:,2)*100;
-plot(tbl(:,1),tbl(:,2),'b.-');
+tbl_1_10000_100 = dlmread('cr_vs_ac_1_10000_100.txt');
+tbl_1_10000_100(:,2) = tbl_1_10000_100(:,2)*100;
+plot(tbl_1_10000_100(:,1),tbl_1_10000_100(:,2),'b.-');
+hold on;
+
+tbl_1_1_100 = dlmread('cr_vs_ac_1_1_100.txt');
+tbl_1_1_100(:,2) = tbl_1_1_100(:,2)*100;
+plot(tbl_1_1_100(:,1),tbl_1_1_100(:,2),'r-d','MarkerFaceColor',[1 0 0]);
+hold on;
+
 ylim([98.8 100]);
 xlabel('call rate (%)');
 ylabel('concordance rate (%)');
-set(gca, 'xdir','reverse');
-
+%set(gca, 'xdir','reverse');
+hl = legend('\beta_1=1, \beta_2=10^4, \beta_3=10^2', ...
+            '\beta_1=1, \beta_2=1, \beta_3=10^2', ...
+            'Location', 'Best');
+set(hl, 'Interpreter', 'tex');
