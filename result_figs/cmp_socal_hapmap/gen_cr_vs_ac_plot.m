e7c67c3339fec2e8765ac55f3ecc90cbce81e15d
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-07-09 13:55:25 -0700

    revised abstract

--- result_figs/cmp_socal_hapmap/gen_cr_vs_ac_plot.m
+++ result_figs/cmp_socal_hapmap/gen_cr_vs_ac_plot.m
@@ -8,7 +8,13 @@ tbl_1_1_100(:,2) = tbl_1_1_100(:,2)*100;
 plot(tbl_1_1_100(:,1),tbl_1_1_100(:,2),'r-d','MarkerFaceColor',[1 0 0]);
 hold on;
 
-ylim([98.8 100]);
+tbl_1_1000000_100 = dlmread('cr_vs_ac_1_1000000_100.txt');
+tbl_1_1000000_100(:,2) = tbl_1_1000000_100(:,2)*100;
+plot(tbl_1_1000000_100(:,1),tbl_1_1000000_100(:,2),'g-v',...
+    'MarkerFaceColor', [0 1 0]);
+hold on;
+
+ylim([96.5 100]);
 xlabel('call rate (%)');
 ylabel('concordance rate (%)');
 %set(gca, 'xdir','reverse');
