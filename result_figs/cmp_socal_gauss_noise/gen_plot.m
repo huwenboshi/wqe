f1db5321246192d224ad8d88a8d5842ae1fbcde3
huwenboshi <shihuwenbo@ucla.edu>
2014-07-09 23:21:14 -0700

    updated result figures

--- result_figs/cmp_socal_gauss_noise/gen_plot.m
+++ result_figs/cmp_socal_gauss_noise/gen_plot.m
@@ -1,4 +1,5 @@
 data = dlmread('result.txt');
+data = data(3:end,:);
 
 vars = data(:,1);
 socal = data(:,2);
@@ -6,6 +7,8 @@ gauss = data(:,3);
 
 h1 = plot(vars,socal,'-b.',vars,gauss,'-rd','MarkerFaceColor',[1 0 0 ]);
 legend('SoCal', 'RLMM');
+xlim([1 10]);
+ylim([88 98.5]);
 xlabel('noise variance');
 ylabel('concordance rate (%)');
 
