4ff24ee209daf308303d127886a274bda28ec90f
huwenboshi <shihuwenbo@ucla.edu>
2014-07-13 23:29:25 -0700

    updated result figure

--- result_figs/cmp_socal_hapmap/gen_cr_vs_ac_plot.m
+++ result_figs/cmp_socal_hapmap/gen_cr_vs_ac_plot.m
@@ -8,10 +8,10 @@ tbl_1_1_100(:,2) = tbl_1_1_100(:,2)*100;
 plot(tbl_1_1_100(:,1),tbl_1_1_100(:,2),'r-d','MarkerFaceColor',[1 0 0]);
 hold on;
 
-tbl_1_100000_100 = dlmread('cr_vs_ac_1_100000_100.txt');
-tbl_1_100000_100(:,2) = tbl_1_100000_100(:,2)*100;
-plot(tbl_1_100000_100(:,1),tbl_1_100000_100(:,2),'-v','Color',[0 0.5 0],...
-    'MarkerFaceColor', [0 0.5 0]);
+tbl_1_1_1000 = dlmread('cr_vs_ac_1_1_1000.txt');
+tbl_1_1_1000(:,2) = tbl_1_1_1000(:,2)*100;
+plot(tbl_1_1_1000(:,1),tbl_1_1_1000(:,2),'c-*',...
+    'MarkerFaceColor', 'c');
 hold on;
 
 tbl_1_1_10 = dlmread('cr_vs_ac_1_1_10.txt');
@@ -20,13 +20,24 @@ plot(tbl_1_1_10(:,1),tbl_1_1_10(:,2),'m-s',...
     'MarkerFaceColor', 'm');
 hold on;
 
+tbl_1_100000_100 = dlmread('cr_vs_ac_1_100000_100.txt');
+tbl_1_100000_100(:,2) = tbl_1_100000_100(:,2)*100;
+plot(tbl_1_100000_100(:,1),tbl_1_100000_100(:,2),'-v','Color',[0 0.5 0],...
+    'MarkerFaceColor', [0 0.5 0]);
+hold on;
+
+
+
+
+
 ylim([98.5 100]);
 xlabel('call rate (%)');
 ylabel('concordance rate (%)');
 %set(gca, 'xdir','reverse');
 hl = legend('\beta_1=1, \beta_2=10^4, \beta_3=10^2', ...
             '\beta_1=1, \beta_2=1, \beta_3=10^2', ...
-            '\beta_1=1, \beta_2=10^5, \beta_3=10^2',...
+            '\beta_1=1, \beta_2=1, \beta_3=10^3',...
             '\beta_1=1, \beta_2=1, \beta_3=10',...
+            '\beta_1=1, \beta_2=10^5, \beta_3=10^2',...
             'Location', 'Best');
 set(hl, 'Interpreter', 'tex');
