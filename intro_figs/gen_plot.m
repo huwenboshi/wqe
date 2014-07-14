ce62da98a0ed579ca3d0284c1e891767e3679889
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-07-14 12:39:05 -0700

    updated intro figure and data

--- intro_figs/gen_plot.m
+++ intro_figs/gen_plot.m
@@ -1,4 +1,6 @@
-data = dlmread('SNP_A-1643081int.txt');
-createfigure_gen_plot(data(:,1),data(:,2));
-title('SNP\_A-1643081');
-% plot(data(:,1),data(:,2),'b.');
+data = dlmread('SNP_A-1721275int.txt');
+%createfigure_gen_plot(data(:,1),data(:,2));
+%plot(data(:,1),data(:,2),'b.');
+%title('SNP\_A-1721275');
+%xlabel('log(allele A intensity)');
+%ylabel('log(allele B intensity)');
