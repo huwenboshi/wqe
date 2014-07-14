0a239b4eeb469687568a08e0baa792f90f9a3ebb
Huwenbo Shi <shihuwenbo@ucla.edu>
2014-07-14 12:22:59 -0700

    updated figures

--- method_figs/gen_defined_cluster.m
+++ method_figs/gen_defined_cluster.m
@@ -42,5 +42,6 @@ ellipse_plot(E_bb, c_bb);
 xlabel('log(allele A intensity)');
 ylabel('log(allele B intensity)');
 xlim([10 15]);
-ylim([9.5 15])
+ylim([9.5 15]);
 axis equal;
+title('SNP\_A-1721275');
--- method_figs/gen_sparse_cluster.m
+++ method_figs/gen_sparse_cluster.m
@@ -44,3 +44,4 @@ ylabel('log(allele B intensity)');
 xlim([10 15]);
 ylim([9.5 15])
 axis equal;
+title('SNP\_A-1700211');
